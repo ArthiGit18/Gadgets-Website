08bae16c877d3d3d204956202f354b0893eea43e
ArthiGit18 <arthiar180@gmail.com>
2024-11-04 10:40:45 +0530

    Navbar and hero Section implemented

--- frontend/src/pages/navbar.js
+++ frontend/src/pages/navbar.js
@@ -1,7 +1,83 @@
-import React from 'react'
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { Button } from '@mui/material';
+import InputBase from '@mui/material/InputBase';
+
+
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
 
 export const Navbar = () => {
-  return (
-    <div>navbar</div>
-  )
+    return (
+        <div className='navbar_wrapper'>
+            <div className='nav_logo'>
+                <img src="assets/logo/Logo (3).png" alt="Logo" />
+            </div>
+            <div className='nav_items'>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">About us</a></li>
+                    <li><a href="/">Product</a></li>
+                    <li><a href="/">Blog</a></li>
+                    <li><a href="/">Contact</a></li>
+                </ul>
+            </div>
+            <div className='nav_actions'>
+                {/* <ShoppingCartIcon /> */}
+                <Search>
+                    <SearchIconWrapper>
+                        <SearchIcon />
+                    </SearchIconWrapper>
+                    <StyledInputBase
+                        placeholder="Search…"
+                        inputProps={{ 'aria-label': 'search' }}
+                    />
+                </Search>
+                <ShoppingCartIcon />
+                <FavoriteIcon />
+                <Button variant='contained'>Sign up/Sign In</Button>
+            </div>
+        </div>
+    )
 }
